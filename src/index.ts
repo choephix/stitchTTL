553f39de9bb1fd4fb268672d087b4bbd736021a7
choephix <i@thechoephix.com>
2021-03-25 11:30:57 +0000

    added index.ts and tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export const stitchTTL = <T extends any[]>(literals: string[], ...substitutions: T) => {
+  return literals.reduce(
+    (result, _, i) => (i >= literals.length - 1 ? result : result + substitutions[i + 1]),
+    literals[0]
+  );
+};
+
+export default stitchTTL;
